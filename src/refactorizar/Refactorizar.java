8653723c94ed2513a110dda3b1b5db9abfef6579
Damián Nogueiras <damiannogueiras@gmail.com>
2014-10-19 18:59:57 +0200

    Lista los números primos de dos cifras. Necesita una refactorización!!!!

--- src/refactorizar/Refactorizar.java
+++ src/refactorizar/Refactorizar.java
@@ -1,21 +1,61 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package refactorizar;
 
 /**
- *
- * @author damian
+ * Lista los numeros primos de dos cifras
+ * 
  */
 public class Refactorizar {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        boolean p = false;
+int numDigitos=0;
+        int ndigitos=0;
+        numDigitos=2;
+        if(numDigitos<=0)
+        System.out.println("Ingrese como parámetro, un numero de digitos correcto (mayor que 0): ");
+        for(int i = 1; i <= 99999; i++ )
+        {
+            int divisionEntera = i;
+ 
+            int contador=0;
+ 
+            while (divisionEntera != 0)
+        {
+            divisionEntera = divisionEntera / 10;
+            contador++;
+        }
+        ndigitos=contador;
+            
+            
+            if(ndigitos==numDigitos){
+                   if (i < 4) p = true;
+            else
+            {
+            if (i % 2 == 0) p = false; 
+                else
+                {
+                    int contador1 = 0;  
+                    int i1 = 1; 
+                    int limite = (i - 1) / 2; 
+                    if  (limite % 2 == 0) limite--;  
+             
+                    while(i1 <= limite)
+                    {
+                        if (i % i1 == 0) contador1++;
+                            i1 += 2;
+                            if (contador1 == 2) i1 = limite + 1;
+                    }
+ 
+        if (contador1 == 1) p = true;
+            } 
+        } 
+     
+                if (p == true) 
+                    System.out.println(i);    
+            }
+        }
     }
     
 }
