9577c4ee60931bbb8ae335b2ae0f3ca2f418aae4
propa <propa@DESKTOP-10ADTBO>
2015-12-09 22:27:22 +0100

    Refac

--- src/refactorizar/Refactorizar.java
+++ src/refactorizar/Refactorizar.java
@@ -1,61 +1,65 @@
-
 package refactorizar;
 
 /**
  * Lista los numeros primos de dos cifras
- * 
+ *
  */
 public class Refactorizar {
 
-    
     public static void main(String[] args) {
         boolean p = false;
-int numDigitos=0;
-        int ndigitos=0;
-        numDigitos=4;
-        if(numDigitos<=0)
-        System.out.println("Ingrese como parámetro, un numero de digitos correcto (mayor que 0): ");
-        for(int i = 1; i <= 99999; i++ )
-        {
-            int divisionEntera = i;
- 
-            int contador=0;
- 
-            while (divisionEntera != 0)
-        {
-            divisionEntera = divisionEntera / 10;
-            contador++;
+        int numDigitos = 0;
+        int ndigitos = 0;
+        numDigitos = 4;
+        if (numDigitos <= 0) {
+            System.out.println("Ingrese como parámetro, un numero de digitos correcto (mayor que 0): ");
         }
-        ndigitos=contador;
-            
-            
-            if(ndigitos==numDigitos){
-                   if (i < 4) p = true;
-            else
-            {
-            if (i % 2 == 0) p = false; 
-                else
-                {
-                    int contador1 = 0;  
-                    int i1 = 1; 
-                    int limite = (i - 1) / 2; 
-                    if  (limite % 2 == 0) limite--;  
-             
-                    while(i1 <= limite)
-                    {
-                        if (i % i1 == 0) contador1++;
+        for (int i = 1; i <= 99999; i++) {
+            int divisionEntera = i;
+
+            int contador = 0;
+
+            while (divisionEntera != 0) {
+                divisionEntera = divisionEntera / 10;
+                contador++;
+            }
+            ndigitos = contador;
+
+            if (ndigitos == numDigitos) {
+                if (i < 4) {
+                    p = true;
+                } else {
+                    if (i % 2 == 0) {
+                        p = false;
+                    } else {
+                        int contador1 = 0;
+                        int i1 = 1;
+                        int limite = (i - 1) / 2;
+                        if (limite % 2 == 0) {
+                            limite--;
+                        }
+
+                        while (i1 <= limite) {
+                            if (i % i1 == 0) {
+                                contador1++;
+                            }
                             i1 += 2;
-                            if (contador1 == 2) i1 = limite + 1;
+                            if (contador1 == 2) {
+                                i1 = limite + 1;
+                            }
+                        }
+
+                        if (contador1 == 1) {
+                            p = true;
+                        }
                     }
- 
-        if (contador1 == 1) p = true;
-            } 
-        } 
-     
-                if (p == true) 
-                    System.out.println(i);    
+                }
+
+                if (p == true) {
+                    System.out.println(i);
+                }
             }
         }
     }
-    
+
 }
